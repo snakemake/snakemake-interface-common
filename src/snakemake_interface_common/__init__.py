91c340f0dd2e98e91412370381a23ce23fd0d090
Johannes Köster <johannes.koester@uni-due.de>
2025-06-23 12:51:47 +0200

    fix: relax version checking by allowing non-semver compatible dev version suffixes (#68)

--- src/snakemake_interface_common/__init__.py
+++ src/snakemake_interface_common/__init__.py
@@ -1,6 +1,3 @@
-import semver
-
-
 def at_least_snakemake_version(version: str) -> bool:
     """Check if the current Snakemake version is at least the specified version.
 
@@ -8,6 +5,7 @@ def at_least_snakemake_version(version: str) -> bool:
     that only compatible interfaces can be installed along with it.
     """
     from snakemake import __version__ as snakemake_version
+    from packaging.version import parse
 
-    snakemake_version = semver.Version.parse(snakemake_version)
-    return snakemake_version >= semver.Version.parse(version)
+    snakemake_version = parse(snakemake_version)
+    return snakemake_version >= parse(version)
