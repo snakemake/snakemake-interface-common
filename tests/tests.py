5f7ae7c440a06e0a5031e9887ac08c02f4c9c8ba
Johannes Koester <johannes.koester@uni-due.de>
2025-06-23 11:02:56 +0200

    fix

--- tests/tests.py
+++ tests/tests.py
@@ -74,5 +74,5 @@ def test_tagged_settings():
 
 
 def test_snakemake_version():
-    assert at_least_snakemake_version("8.1")
-    assert not at_least_snakemake_version("100.0")
+    assert at_least_snakemake_version("8.1.0")
+    assert not at_least_snakemake_version("100.0.0")
