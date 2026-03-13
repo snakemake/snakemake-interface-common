b64dcf6fdb1b810a09de0b5cbbd09da4cd4f9886
Jonas Hoersch <jonas.hoersch@openenergytransition.org>
2026-03-13 14:49:57 +0100

    feat: add RuleCacheInterface

--- src/snakemake_interface_common/rules.py
+++ src/snakemake_interface_common/rules.py
@@ -1,4 +1,28 @@
 from abc import ABC, abstractmethod
+from typing import Optional
+
+
+class RuleCacheInterface(ABC):
+    @property
+    @abstractmethod
+    def output(self) -> bool: ...
+
+    @property
+    @abstractmethod
+    def omit_software(self) -> bool: ...
+
+    @property
+    @abstractmethod
+    def omit_storage_content(self) -> bool: ...
+
+    @abstractmethod
+    async def exists(self, job) -> bool: ...
+
+    @abstractmethod
+    async def fetch(self, job): ...
+
+    @abstractmethod
+    async def store(self, job): ...
 
 
 class RuleInterface(ABC):
@@ -13,3 +37,7 @@ class RuleInterface(ABC):
     @property
     @abstractmethod
     def snakefile(self) -> str: ...
+
+    @property
+    @abstractmethod
+    def cache(self) -> Optional[RuleCacheInterface]: ...
